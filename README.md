38540fc50b0a2e4e1ea0b0435ec248cf30936dab
morriswinkler <m.winkler@open3dengineering.org>
2017-12-10 23:48:12 +0100

    rename to ssh-shield
    Changes:
        rename package
        improve documentation
        improve error logging

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# ssh-guard
+# ssh-shield
 --
-Command ssh-guard is a simple tool to manage allowed ssh commands via the
+Command ssh-shield is a simple too to manage allowed ssh commands via the
 authorized_keys command parameter.
 
 ### Install
 
-    go get github.com/morriswinkler/ssh-guard
+    go get github.com/morriswinkler/ssh-shield
 
 
 ### Setup
@@ -18,36 +18,38 @@ id would be at:
 Prepend the key with the command parameter:
 
     cat /home/morriswinkler/.ssh/authorized_keys
-    commadn="/home/morriswinkler/go/bin/ssh-guard" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyGB4+u1qRBNOpDGtQm1LgJXJMmRo+Dvu4WKbpwq29aSM+1KulQw+sJ9vhpKXZt5bqCCkv/2W+ScqSBP87AaFqT8tQ45f4tq6IYibYLjWT492qL948B7Yd2EEvVmP1K81uPvLLzgiuZ3Ci/1pa7kBEmxqI7itrD7g1A9BRixq74X3S/KvhEti/Nm8BGQBrg+8h05qyHG7qtQtwajbQDZsxAEN3OseZpI2n0WFBcJ84ic5lK8f01CBtRLPvwcu8/lpn7bW5MzC0ShyBT1OMBaUwzwfAfn9Tw9aoziAzmGFbW5OkuBObQKG6pSo2Th2C40fhTO1WoefHv2FT4BxhgpVv morriswinkler@ssh_server
+    command="/home/morriswinkler/go/bin/ssh-shield" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyGB4+u1qRBNOpDGtQm1LgJXJMmRo+Dvu4WKbpwq29aSM+1KulQw+sJ9vhpKXZt5bqCCkv/2W+ScqSBP87AaFqT8tQ45f4tq6IYibYLjWT492qL948B7Yd2EEvVmP1K81uPvLLzgiuZ3Ci/1pa7kBEmxqI7itrD7g1A9BRixq74X3S/KvhEti/Nm8BGQBrg+8h05qyHG7qtQtwajbQDZsxAEN3OseZpI2n0WFBcJ84ic5lK8f01CBtRLPvwcu8/lpn7bW5MzC0ShyBT1OMBaUwzwfAfn9Tw9aoziAzmGFbW5OkuBObQKG6pSo2Th2C40fhTO1WoefHv2FT4BxhgpVv morriswinkler@ssh_server
 
-Some options to make it a bit more secure:
+Add more options to make it more secure:
 
     cat /home/morriswinkler/.ssh/authorized_keys
-    from="8.8.8.8",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty,commadn="/home/morriswinkler/go/bin/ssh-guard" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyGB4+u1qRBNOpDGtQm1LgJXJMmRo+Dvu4WKbpwq29aSM+1KulQw+sJ9vhpKXZt5bqCCkv/2W+ScqSBP87AaFqT8tQ45f4tq6IYibYLjWT492qL948B7Yd2EEvVmP1K81uPvLLzgiuZ3Ci/1pa7kBEmxqI7itrD7g1A9BRixq74X3S/KvhEti/Nm8BGQBrg+8h05qyHG7qtQtwajbQDZsxAEN3OseZpI2n0WFBcJ84ic5lK8f01CBtRLPvwcu8/lpn7bW5MzC0ShyBT1OMBaUwzwfAfn9Tw9aoziAzmGFbW5OkuBObQKG6pSo2Th2C40fhTO1WoefHv2FT4BxhgpVv morriswinkler@ssh_server
+    from="8.8.8.8",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty,command="/home/morriswinkler/go/bin/ssh-shield" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyGB4+u1qRBNOpDGtQm1LgJXJMmRo+Dvu4WKbpwq29aSM+1KulQw+sJ9vhpKXZt5bqCCkv/2W+ScqSBP87AaFqT8tQ45f4tq6IYibYLjWT492qL948B7Yd2EEvVmP1K81uPvLLzgiuZ3Ci/1pa7kBEmxqI7itrD7g1A9BRixq74X3S/KvhEti/Nm8BGQBrg+8h05qyHG7qtQtwajbQDZsxAEN3OseZpI2n0WFBcJ84ic5lK8f01CBtRLPvwcu8/lpn7bW5MzC0ShyBT1OMBaUwzwfAfn9Tw9aoziAzmGFbW5OkuBObQKG6pSo2Th2C40fhTO1WoefHv2FT4BxhgpVv morriswinkler@ssh_server
 
-Thats it you are ready to roll.
+Thats it.
 
 
 ### CommandManagement
 
 By default no command is allowed, you can add commands by running:
 
-    ssh-guard -add "ls /"
+    ssh-shield -add "ls /"
 
 To list them use:
 
-    ssh-guard -list
+    ssh-shield -list
 
 And finaly to remove them use:
 
-    ssh-guard -del 1
+    ssh-shield -del 1
 
-Commands are stored as a colon separated list in ~/.config/ssh-guard/config.ini:
+Commands are stored as a colon separated list in
+~/.config/ssh-shield/config.ini:
 
     allowed_commands = ls /: ps aux:ls
 
-The log file contains ignored and allowed commands and invoations from the sshd,
-you can use it to find commands to add if needed. You can change the location
-inside the config.
+The log file contains ignored and allowed command invocations from the sshd, it
+can be used to find matching command signatures or track missusage.
 
-    logfile = ~/ssh-gaurd.log
+The default location can be changed in the config.
+
+    logfile = ~/ssh-shield.log
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 /*
-Command ssh-guard is a simple too to manage allowed ssh commands via the authorized_keys command parameter.
+Command ssh-shield is a simple too to manage allowed ssh commands via the authorized_keys command parameter.
 
 Install
 
-       go get github.com/morriswinkler/ssh-guard
+       go get github.com/morriswinkler/ssh-shield
 
 Setup
 
@@ -14,12 +14,12 @@ Upload a public ssh rsa key to the user folder on your ssh server, for my user i
 Prepend the key with the command parameter:
 
       cat /home/morriswinkler/.ssh/authorized_keys
-      commadn="/home/morriswinkler/go/bin/ssh-guard" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyGB4+u1qRBNOpDGtQm1LgJXJMmRo+Dvu4WKbpwq29aSM+1KulQw+sJ9vhpKXZt5bqCCkv/2W+ScqSBP87AaFqT8tQ45f4tq6IYibYLjWT492qL948B7Yd2EEvVmP1K81uPvLLzgiuZ3Ci/1pa7kBEmxqI7itrD7g1A9BRixq74X3S/KvhEti/Nm8BGQBrg+8h05qyHG7qtQtwajbQDZsxAEN3OseZpI2n0WFBcJ84ic5lK8f01CBtRLPvwcu8/lpn7bW5MzC0ShyBT1OMBaUwzwfAfn9Tw9aoziAzmGFbW5OkuBObQKG6pSo2Th2C40fhTO1WoefHv2FT4BxhgpVv morriswinkler@ssh_server
+      command="/home/morriswinkler/go/bin/ssh-shield" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyGB4+u1qRBNOpDGtQm1LgJXJMmRo+Dvu4WKbpwq29aSM+1KulQw+sJ9vhpKXZt5bqCCkv/2W+ScqSBP87AaFqT8tQ45f4tq6IYibYLjWT492qL948B7Yd2EEvVmP1K81uPvLLzgiuZ3Ci/1pa7kBEmxqI7itrD7g1A9BRixq74X3S/KvhEti/Nm8BGQBrg+8h05qyHG7qtQtwajbQDZsxAEN3OseZpI2n0WFBcJ84ic5lK8f01CBtRLPvwcu8/lpn7bW5MzC0ShyBT1OMBaUwzwfAfn9Tw9aoziAzmGFbW5OkuBObQKG6pSo2Th2C40fhTO1WoefHv2FT4BxhgpVv morriswinkler@ssh_server
 
 Add more options to make it more secure:
 
       cat /home/morriswinkler/.ssh/authorized_keys
-      from="8.8.8.8",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty,commadn="/home/morriswinkler/go/bin/ssh-guard" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyGB4+u1qRBNOpDGtQm1LgJXJMmRo+Dvu4WKbpwq29aSM+1KulQw+sJ9vhpKXZt5bqCCkv/2W+ScqSBP87AaFqT8tQ45f4tq6IYibYLjWT492qL948B7Yd2EEvVmP1K81uPvLLzgiuZ3Ci/1pa7kBEmxqI7itrD7g1A9BRixq74X3S/KvhEti/Nm8BGQBrg+8h05qyHG7qtQtwajbQDZsxAEN3OseZpI2n0WFBcJ84ic5lK8f01CBtRLPvwcu8/lpn7bW5MzC0ShyBT1OMBaUwzwfAfn9Tw9aoziAzmGFbW5OkuBObQKG6pSo2Th2C40fhTO1WoefHv2FT4BxhgpVv morriswinkler@ssh_server
+      from="8.8.8.8",no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty,command="/home/morriswinkler/go/bin/ssh-shield" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDyGB4+u1qRBNOpDGtQm1LgJXJMmRo+Dvu4WKbpwq29aSM+1KulQw+sJ9vhpKXZt5bqCCkv/2W+ScqSBP87AaFqT8tQ45f4tq6IYibYLjWT492qL948B7Yd2EEvVmP1K81uPvLLzgiuZ3Ci/1pa7kBEmxqI7itrD7g1A9BRixq74X3S/KvhEti/Nm8BGQBrg+8h05qyHG7qtQtwajbQDZsxAEN3OseZpI2n0WFBcJ84ic5lK8f01CBtRLPvwcu8/lpn7bW5MzC0ShyBT1OMBaUwzwfAfn9Tw9aoziAzmGFbW5OkuBObQKG6pSo2Th2C40fhTO1WoefHv2FT4BxhgpVv morriswinkler@ssh_server
 
 Thats it.
 
@@ -27,24 +27,25 @@ CommandManagement
 
 By default no command is allowed, you can add commands by running:
 
-         ssh-guard -add "ls /"
+         ssh-shield -add "ls /"
 
 To list them use:
 
-         ssh-guard -list
+         ssh-shield -list
 
 And finaly to remove them use:
 
-        ssh-guard -del 1
+        ssh-shield -del 1
 
-Commands are stored as a colon separated list in ~/.config/ssh-guard/config.ini:
+Commands are stored as a colon separated list in ~/.config/ssh-shield/config.ini:
 
        allowed_commands = ls /: ps aux:ls
 
-The log file contains ignored and allowed commands and invoations from the sshd, you can use it to find commands to add if needed.
-You can change the location inside the config.
+The log file contains ignored and allowed command invocations from the sshd, it can be used to find matching command signatures or track missusage.
 
-      logfile = ~/ssh-gaurd.log
+The default location can be changed in the config.
+
+      logfile = ~/ssh-shield.log
 */
 package main
 
@@ -60,7 +61,7 @@ import (
 )
 
 var (
-	logFileName = flag.String("logfile", "~/log/ssh-guard.log", "logfile path")
+	logFileName = flag.String("logfile", "~/ssh-shield.log", "logfile path")
 	cmds        = flag.String("allowd_commands", "", "Colon separated list of allowed commads, use add / del / list")
 	addCMD      = flag.String("add", "", "add command")
 	delCMD      = flag.Int("del", 0, "del command")
@@ -139,7 +140,7 @@ func cmdLine(conf *globalconf.GlobalConf) {
 
 func main() {
 
-	conf, err := globalconf.New("ssh-guard")
+	conf, err := globalconf.New("ssh-shield")
 	if err != nil {
 		clog.Fatal(err)
 	}
@@ -147,12 +148,13 @@ func main() {
 
 	logFile, err := os.OpenFile(*logFileName, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
 	if err != nil {
-		clog.Fatal(err)
+		if os.IsNotExist(err) {
+			clog.Errorf("Could not create log file %s: \n Make sure the foler exist and is writable, if neccesary change the location in the config file %s to logfile = /your/path.log", *logFileName, conf.Filename)
+		} else {
+			clog.Fatal(err)
+		}
 	}
 	defer logFile.Close()
-
-	//w := bufio.NewWriter(logFile)
-	//defer w.Flush()
 
 	clog.LogFile(logFile)
 
